1acee3026100cf1103679caadf453dc8ec5f2061
hazratamin98 <hazratamin98@gmail.com>
2022-03-09 23:24:41 +0500

    404 error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
-  return <div className="App">Hello word</div>
+  return <div className="App">Hello word </div>
 }
 
 export default App
