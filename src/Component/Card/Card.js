cfcf70084286752a7b98f6f19b953bdd9545fb40
hazratamin98 <hazratamin98@gmail.com>
2022-03-10 18:01:06 +0500

    Axios setup

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react"
-import axios, { Axios } from "axios"
+import axios from "axios"
 import classNames from "classnames/bind"
 import Coin from "./Coin"
 
